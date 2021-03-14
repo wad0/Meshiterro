1a061c3ded18887a455c8813622441ddc5e5a223
wad0 <fukushima0102@gmail.com>
2021-03-14 17:07:42 +0000

    0315_1

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,9 +16,14 @@ class PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
   end
 
    private
