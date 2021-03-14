726cde6b5aa5fd6ade169cb9471f7b16225f1e5d
wad0 <fukushima0102@gmail.com>
2021-03-14 12:08:12 +0000

    0314_1

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd2c88e78650546aa60007a9581411a39aed49db559626dbe12c6bfb5b49a55f3af1febafb78928b6dccc83a1b7f98868c12116a5fa3f9ecc8beb62f5e6182a99'
